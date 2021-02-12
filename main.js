6abb4837c62cd25deb2220ba5831432e98a7b33a
Simon Smesnik <simon.a.smesnik@gmail.com>
2021-02-12 14:36:50 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const imageContainer = document.getElementById("imageContainer");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("previous");
+
+let pointer = 1;
+
+nextBtn.addEventListener("click", () => {
+    switch (pointer){
+        case 1:
+            imageContainer.style.transform = "translateX(-101%)";
+            break;
+        case 2:
+            imageContainer.style.transform = "translateX(-202%)";
+            break;
+        case 3:
+            imageContainer.style.transform = "translateX(-302%)";
+            break;
+        case 4:
+            imageContainer.style.transform = "translateX(0%)";
+            pointer = 0;
+            break;
+    }
+    pointer++;
+})
+
+prevBtn.addEventListener("click", () => {
+    switch (pointer){
+        case 1:
+            imageContainer.style.transform = "translateX(-302%)";
+            pointer = 4;
+            break;
+        case 2:
+            imageContainer.style.transform = "translateX(0%)";
+            break;
+        case 3:
+            imageContainer.style.transform = "translateX(-101%)";
+            break;
+        case 4:
+            imageContainer.style.transform = "translateX(-202%)";
+            break;
+    }
+    pointer--;
+})
